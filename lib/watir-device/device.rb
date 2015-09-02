ede93983093afb351088d56580c2630dfc047acd
Johnson Denen <johnson.denen@gmail.com>
2015-09-02 15:33:07 -0400

    Add documentation

--- lib/watir-device/device.rb
+++ lib/watir-device/device.rb
@@ -1,25 +1,56 @@
 require 'watir-webdriver'
 
 module Watir
+
+  #
+  # Automate Chrome in mobile emulation mode. Devices availabe for emulation are available in the 
+  # Dev Tool preferences, including "Apple iPhone 6 Plus", "Samsung Galaxy Note 3", "Google Nexus 10", etc.
+  #
+  # @author Johnson Denen
+  #
   class Device
     attr_reader :device_name, :driver, :browser
     
     class << self
+      
+      #
+      # Spin up an instance and navigate to a URL.
+      #
+      # @example Open iPhone 6+ to google.com
+      #   Watir::Device.start "www.google.com", "Apple iPhone 6 Plus"
+      #
+      # @param url [String] URL to visit
+      # @param device_name [String] device to emulate
+      # @return [Watir::Device]
+      #
       def start url, device_name, *args
         new(device_name, *args).tap { |dev| dev.goto url }
       end
     end
 
+    #
+    # Emulate a device browser with Chrome's mobile emulation tool.
+    #
+    # @param device_name [String] device to emulate
+    # @return [Watir::Device]
+    #
     def initialize device_name, *args
       @driver = device_driver(device_name)
       @device_name = device_name
       @browser = Watir::Browser.new driver
     end
 
+    #
+    # Get the emulated device's name.
+    #
+    # @return [String]
+    #
     def device
       device_name
     end
 
+    # Build a WebDriver to emulate the device.
+    # @api private    
     def device_driver name
       opts = {
         "mobileEmulation" => {
@@ -30,8 +61,9 @@ module Watir
       Selenium::WebDriver.for(:chrome, desired_capabilities: capabilities)
     end
 
-    private
-    
+    # Undefined methods are passed to Watir::Browser.
+    # @see https://github.com/watir/watir-webdriver/blob/master/lib/watir-webdriver/browser.rb Watir::Browser
+    # @api private    
     def method_missing mthd, *args
       browser.send(mthd, *args)
     end
