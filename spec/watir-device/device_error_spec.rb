6bfa78e4030febd30d16b7b2b08f96d47ec7f02a
Johnson Denen <johnson.denen@gmail.com>
2015-09-03 14:12:34 -0400

    Mock out chromedriver for TravisCI

--- spec/watir-device/device_error_spec.rb
+++ spec/watir-device/device_error_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Watir::DeviceError do
   context "when Watir::Device#new is passed an invalid device name" do
     it "raises a DeviceError" do
+      expect(Selenium::WebDriver).to receive(:for).and_raise(Selenium::WebDriver::Error::UnknownError)
       expect{ Watir::Device.new "Foo" }.to raise_error(Watir::DeviceError)
     end
   end
